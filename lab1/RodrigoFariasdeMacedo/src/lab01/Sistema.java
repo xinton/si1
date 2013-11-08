a77cbe7aa93d493dbd91c96dcb96bb4b43bdde45
rodrigofariasm <rodrigohfarias@gmail.com>
2013-11-08 17:19:51 -0300

    lab1 terminado, espero

---
+++ lab1/RodrigoFariasdeMacedo/src/lab01/Sistema.java
@@ -0,0 +1,25 @@
+package lab01;
+import java.util.Scanner;
+public class Sistema {
+
+	/**
+	 * @param args
+	 */
+		private static Escritor wter;
+	public static void main(String[] args) throws Exception{
+		Scanner entrada = new Scanner(System.in);
+		wter = new Escritor();
+		System.out.println("Informe um número");
+		
+		String entry = entrada.next();
+		try{
+			wter.setInput(entry);
+			System.out.println(wter.geraNumero());
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+			main(args);
+		}
+		
+	}
+
+}
