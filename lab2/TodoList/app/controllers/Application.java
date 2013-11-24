602adfb4d23ced069a2a54f462907cb02d8c888a
rodrigofariasm <rodrigohfarias@gmail.com>
2013-11-24 13:59:13 -0300

    pagina /tasks funcionando

--- lab2/TodoList/app/controllers/Application.java
+++ lab2/TodoList/app/controllers/Application.java
@@ -2,16 +2,18 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
+import play.data.*;
 import views.html.*;
 
 public class Application extends Controller {
-
+	
+	static Form<Task> taskForm = Form.form(Task.class);
+	
     public static Result index() {
         return redirect(routes.Application.tasks());
     }
     public static Result tasks() {
-        return TODO;
+    	return ok(views.html.index.render(Task.all(), taskForm));
     }
     public static Result newTask() {
         return TODO;
